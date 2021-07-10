23016f2ca674c50b2dfc521555566f4929ac2d6a
Hassanfcs <ibn.e.hasssan@gmail.com>
2021-07-11 03:15:18 +0500

    generalize the winning strategy

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,26 +8,32 @@ export default class Game extends Component {
     this.state = {
       xIsNext: true,
       stepNumber: 0,
+      size: 5,
       history: [
         { squares: Array(9).fill(null) }
       ]
     }
+  }
+  squareSize = ()=> {
+    let s = this.state.size
+    return s*s
   }
   resetGame() {
     this.setState({
       xIsNext: true,
       stepNumber: 0,
       history: [
-        { squares: Array(9).fill(null) }
+        { squares: Array(this.squareSize()).fill(null) }
       ]
     })
   }
 
   handleClick(i) {
+    const size = this.state.size
     const history = this.state.history.slice(0, this.state.stepNumber+1);
     const current = history[history.length-1];
     const squares = current.squares.slice();
-    const winner = decideWinner(squares);
+    const winner = decideWinner(squares, size);
     if (winner || squares[i]) {
       return;
     }
@@ -42,9 +48,10 @@ export default class Game extends Component {
   }
 
   render() {
+    const size = this.state.size
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = decideWinner(current.squares);
+    const winner = decideWinner(current.squares, size);
     const restartBtn = ()=>{
       return(
           <div class='restart-btn' onClick={()=>{this.resetGame()}}>
@@ -60,7 +67,7 @@ export default class Game extends Component {
       if(winner) {
         status = 'Winner is ' + winner
       }
-      else if (history.length > 9) {
+      else if (history.length > this.squareSize()) {
         status = 'Game is Tied'
       }
       else {
@@ -86,21 +93,59 @@ export default class Game extends Component {
   }
 }
 
-function decideWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
+function winningPositions(){
+  let size = 5
+  let positions = []
+  //row wise winning posititons
+  for(let i=0;i<size; i++){
+    let row = []
+    for(let j=0;j<size; j++){
+      row.push(i*size+j)
+    }
+    positions.push(row)
+  }
+  //col wise winning positions
+  for(let i=0;i<size; i++){
+    let col = []
+    for(let j=0;j<size; j++){
+      col.push(j*size+i)
+    }
+    positions.push(col)
+  }
+  //left diagonal winning positions
+  let ldig = []
+  for(let i=0;i<size; i++){
+    ldig.push(i*size+i)
+  }
+  positions.push(ldig)
+  //right diagonal winning positions
+  let rdig = []
+  for(let i=0; i<size; i++){
+    rdig.push(i*size+(size-1-i))
+  }
+  positions.push(rdig)
+  return  positions
+}
+
+function decideWinner(squares, size) {
+  const lines = winningPositions()
+
   for(let i=0; i<lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c])
-      return squares[a]
+    if(size == 3){
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c])
+        return squares[a]
+    }
+    else if(size == 4){
+      const [a, b, c, d] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c] && squares[c] === squares[d])
+        return squares[a]
+    }
+    else if(size == 5){
+      const [a, b, c, d, e] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c] && squares[c] === squares[d] && squares[d] === squares[e])
+        return squares[a]
+    }
   }
   return null;
 }
