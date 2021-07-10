8a561f7bee8b9e4785c10de40e99b8ff79d3ef66
Hassanfcs <ibn.e.hasssan@gmail.com>
2021-07-10 18:06:06 +0500

    done with filling the boxes

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,12 +5,25 @@ export default class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cIsNext: true,
+      xIsNext: true,
       stepNumber: 0,
       history: [
         { squares: Array(9).fill(null) }
       ]
     }
+  }
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber+1);
+    const current = history[history.length-1];
+    const squares = current.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat({
+        squares: squares
+      }),
+      xIsNext: !this.state.xIsNext,
+      stepNumber: history.length
+    })
   }
   render() {
     const history = this.state.history;
@@ -19,7 +32,7 @@ export default class Game extends Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board onClick = {(i)=>this.onClick(i)}
+          <Board onClick = {(i)=>this.handleClick(i)}
           squares={current.squares}
           />
         </div>
